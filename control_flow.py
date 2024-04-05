cbb0bd4ae3fc909b48c9d9f2d5bd9a08876f2bc6
susan.mamu <susan.mamu99@gmail.com>
2024-04-05 12:04:50 +0300

    added Numpy tutorial

---
+++ control_flow.py
@@ -0,0 +1,13 @@
+x = 0
+
+while x < 10:
+    print("x:", x)
+    if x == 5:
+        print("Breaking...")
+        break
+    x += 1
+
+print("End")
+for letter in "Python": # continue when letter is 'h’ 
+	if letter == "h": 	continue 
+print("Current Letter :", letter) 
---
+++ practice.py
@@ -0,0 +1,28 @@
+# if elif and else statements
+temperature = 20
+if temperature >24:
+    print("It is hot outside")
+
+elif temperature < 24:
+    print("It is cold cold outside")
+
+elif temperature == 24:
+    print("Temperatures are okay today")
+
+else:
+    print("The weather in un understandable")
+
+# or operator
+age = 25
+
+if age < 18 or age >= 65:
+    print("You are either a minor or a senior citizen")
+
+
+#  not operator Example:
+is_raining = True
+
+if not is_raining:
+    print("It's not raining")
+
+
